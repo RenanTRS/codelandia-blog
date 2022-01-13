2d5fd671a468656b9a33cf059b5023b6e7dd88c6
Renan Souza <renantrsouza@gmail.com>
2022-01-13 15:43:23 -0400

    Create home component

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import { Header } from '../components/Header';
+import { Title } from '../components/Header/Title';
+import { SubTitle } from '../components/Header/SubTitle';
+import { FieldSearch } from '../components/Header/FieldSearch';
 
 export function Home(){
     return (
-        <Header />
+        <Header>
+            <Title>Codelândia</Title>
+            <SubTitle>blog</SubTitle>
+            <FieldSearch />
+        </Header>
     );
 }
