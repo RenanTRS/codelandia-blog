c6f208e65706f9edcd39fa02cb18eedcc8bce5d9
Renan Souza <renantrsouza@gmail.com>
2022-01-13 16:37:53 -0400

    add style for input

--- src/components/Header/Field/Input/index.ts
+++ src/components/Header/Field/Input/index.ts
@@ -1,4 +1,23 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
+    font-family: 'Inter', sans-serif;
+    font-size: 1.4rem;
+    color: #ffffff;
+
+    flex: 1;
+
+    height: 4rem;
+
+    background: transparent;
+
+    border: none;
+
+    margin-left: 1.1rem;
+
+    outline: none;
+
+    &::placeholder{
+        color: #fff;
+    }
 `;
--- src/components/Header/Field/index.ts
+++ src/components/Header/Field/index.ts
@@ -2,4 +2,15 @@ import styled from 'styled-components';
 
 export const Field = styled.div`
     display: flex;
+    align-items: center;
+
+    background: rgba(255, 255, 255, 0.2);
+
+    width: 100%;
+
+    padding: 0 .7rem;
+
+    border-radius: .5rem;
+
+    overflow: hidden;
 `;
