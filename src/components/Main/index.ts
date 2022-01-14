1cafc350bad569f600c8ec66fc58cadaf5c2ccba
Renan Souza <renantrsouza@gmail.com>
2022-01-14 12:56:40 -0400

    add limit of input

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -1,2 +1,6 @@
 import styled from 'styled-components';
 
+export const Main = styled.main`
+    background: #F3F5F7;
+`;
+
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -6,17 +6,22 @@ import { Title } from '../components/Header/Title';
 import { FieldSearch } from '../components/Header/FieldSearch';
 import { FirstTitle } from '../components/Header/FirstTitle';
 import { SubTitle } from '../components/Header/SubTitle';
+import { Main } from '../components/Main';
 
 export function Home(){
     return (
-        <Header>
-            <Container>
-                <Title>
-                    <FirstTitle>Codelândia</FirstTitle>
-                    <SubTitle>blog</SubTitle>
-                </Title>
-                <FieldSearch />
-            </Container>
-        </Header>
+        <>
+            <Header>
+                <Container>
+                    <Title>
+                        <FirstTitle>Codelândia</FirstTitle>
+                        <SubTitle>blog</SubTitle>
+                    </Title>
+                    <FieldSearch />
+                </Container>
+            </Header>
+            <Main>
+            </Main>
+        </>
     );
 }
