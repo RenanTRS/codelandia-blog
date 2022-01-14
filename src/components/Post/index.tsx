c09de5842dbd9a66570e0521d801a5239ced8c05
Renan Souza <renantrsouza@gmail.com>
2022-01-14 15:05:54 -0400

    Create Post component

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import fakeData from '../../fakeData.json';
+
+import { CardPost } from '../CardPost';
+
+export const Post = () => {
+    return (
+        <>
+          {fakeData.map((val, key, []) =>{
+            return (
+                <CardPost date={val.date} title={val.title} text={val.text} key={key} />
+            );
+          })}
+        </>
+    );
+}
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { Header } from '../components/Header';
 import { Container } from '../components/Header/Container';
 import { Title } from '../components/Header/Title';
-import { FieldSearch } from '../components/Header/FieldSearch';
 import { FirstTitle } from '../components/Header/FirstTitle';
 import { SubTitle } from '../components/Header/SubTitle';
+import { FieldSearch } from '../components/Header/FieldSearch';
 import { Main } from '../components/Main';
+import { Post } from '../components/Post';
 
 export function Home(){
     return (
@@ -21,6 +22,7 @@ export function Home(){
                 </Container>
             </Header>
             <Main>
+                <Post />
             </Main>
         </>
     );
