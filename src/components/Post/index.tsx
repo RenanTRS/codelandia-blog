87f2487824bbb805f6b206b963cebdab9c2f7d5c
Renan Souza <renantrsouza@gmail.com>
2022-01-14 23:20:25 -0400

    fixed bug

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -7,7 +7,7 @@ import { CardPost } from '../CardPost';
 export const Post = () => {
     return (
         <>
-          {fakeData.map((val, key, []) =>{
+          {fakeData.map((val, key) =>{
             return (
                 <CardPost date={val.date} title={val.title} text={val.text} key={key} />
             );
