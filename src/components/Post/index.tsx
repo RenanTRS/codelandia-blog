70f102941e8c3c7a84d1db8086bf11f05c7841b3
Renan Souza <renantrsouza@gmail.com>
2022-01-21 14:38:04 -0400

    add filter

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {SearchContext} from '../../context/SearchContext'
 
 import fakeData from '../../fakeData.json';
 
 import { CardPost } from '../CardPost';
 
 export const Post = () => {
+  const {search} = useContext(SearchContext);
     return (
         <>
-          {fakeData.map((val, key) =>{
+          {fakeData.filter((val) => {
+            if(search === ''){
+              return val;
+            } else if (val.title.toLowerCase().includes(search.toLowerCase())){
+              //Se o título em minúsculo for igual ao state em minúsculo
+              return val;
+            }
+          }).map((val, key) =>{
             return (
                 <CardPost date={val.date} title={val.title} text={val.text} key={key} />
             );
