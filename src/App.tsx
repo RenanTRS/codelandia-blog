accfe1b40d3c33430de2d0cf796d703d5607ecca
Renan Souza <renantrsouza@gmail.com>
2022-01-22 13:59:45 -0400

    Create Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { SearchContextProvider } from './context/SearchContext';
 
-
 import { Home } from './page/Home';
+import { Post } from './page/Post';
 
 function App() {
   return (  
-    <SearchContextProvider>
-      <Home />
-    </SearchContextProvider>
+    <Router>
+      <SearchContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/post/:id' element={<Post />} />
+        </Routes>
+      </SearchContextProvider>
+    </Router>
   );
 }
 
