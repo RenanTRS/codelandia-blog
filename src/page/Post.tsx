ca38b8b7c20d7a03ddf8156edea65b28b0749d50
Renan Souza <renantrsouza@gmail.com>
2022-01-24 13:02:54 -0400

    Receive array at key position

--- src/page/Post.tsx
+++ src/page/Post.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export const Post = () => {
-    let {id} = useParams();
+    const {id} = useParams();
+    const num = Number(id);
+    const data = fakeData[num];
+    
     return(
         <h1>{id}</h1>
     );
