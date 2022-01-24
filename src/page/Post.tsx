9f00d40fbdd4d22389e5b37c913a756213f4be47
Renan Souza <renantrsouza@gmail.com>
2022-01-24 13:03:08 -0400

    add styles

--- src/page/Post.tsx
+++ src/page/Post.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import fakeData from '../fakeData.json';
+
+import { ContainerPost } from '../components/Posts/ContainerPost';
+import { Card } from '../components/CardPost/Card';
+import { DatePost } from '../components/Posts/DatePost';
+import { TitlesPost } from '../components/Posts/TitlesPost';
+import { Text } from '../components/CardPost/Text';
+
 
 export const Post = () => {
     const {id} = useParams();
@@ -7,6 +15,12 @@ export const Post = () => {
     const data = fakeData[num];
     
     return(
-        <h1>{id}</h1>
+        <ContainerPost>
+            <Card>
+                <DatePost>{data.date}</DatePost>
+                <TitlesPost>{data.title}</TitlesPost>
+                <Text>{data.text}</Text>
+            </Card>
+        </ContainerPost>
     );
 }
