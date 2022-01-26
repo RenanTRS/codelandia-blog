6161605c5bf6f30a677e57e65d5cb8a5dfeb5bb4
Renan Souza <renantrsouza@gmail.com>
2022-01-26 14:45:54 -0400

    Add like effect

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from './Card';
 import { DateContainer } from './DateContainer';
 import { SpanDate } from './SpanDate';
 import HeartImg from '../../assets/img/heart.svg'
+import HeartLikeImg from '../../assets/img/heartLike.svg'
 import { TextContainer } from './TextContainer';
 import { TitlePost } from './TitlePost';
 import { Description } from './Description';
@@ -12,20 +13,32 @@ interface cardProps {
     date?: string;
     title?: string;
     description?: string;
-    key?: number;
+    index?: number;
+    key: number;
 }
 
 export const CardPost = (props: cardProps) => {
+
+    const [like, setLike] = useState<boolean>(false);
+
+    function handleLike(){
+        setLike(!like)
+    }
     return(
         <Card>
             <DateContainer>
                 <SpanDate>{props.date}</SpanDate>
-                <img src={HeartImg} alt="heart" />
+                {!like? 
+                    <img src={HeartImg} alt="heart" onClick={handleLike}/>:
+                    <img src={HeartLikeImg} alt="heart" onClick={handleLike}/>
+                }
             </DateContainer>
-            <TextContainer>
-                <TitlePost>{props.title}</TitlePost>
-                <Description>{props.description}</Description>
-            </TextContainer>
+            <Link to={`/post/${props.index}`}>
+                <TextContainer>
+                    <TitlePost>{props.title}</TitlePost>
+                    <Description>{props.description}</Description>
+                </TextContainer>
+            </Link>
         </Card>
     );
 }
